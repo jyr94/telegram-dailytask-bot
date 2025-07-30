b3cf0b52a2193dbc3c99c74d3de51d0f3408855a
jer yulfiranda <jeriyulfiranda94@gmail.com>
2025-07-30 09:44:08 +0700

    feat: add firebase config base64

--- config/config.go
+++ config/config.go
@@ -2,6 +2,8 @@
 package config
 
 import (
+	"encoding/base64"
+	"fmt"
 	"log"
 	"os"
 
@@ -13,6 +15,7 @@ type Config struct {
 	GoogleSheetID     string
 	CredentialsJSON   string
 	FirebaseProjectId string
+	CredentialsBase64 string
 }
 
 func Load() Config {
@@ -24,9 +27,28 @@ func Load() Config {
 		TelegramToken:     os.Getenv("TELEGRAM_TOKEN"),
 		CredentialsJSON:   os.Getenv("GOOGLE_CREDENTIALS_FILE"),
 		FirebaseProjectId: os.Getenv("FIREBASE_PROJECT_ID"),
+		CredentialsBase64: os.Getenv("GOOGLE_CREDENTIALS_BASE64"),
 	}
+	filepath := WriteCredentialsToTempFile(cfg.CredentialsBase64)
+	fmt.Println(filepath)
+	cfg.CredentialsJSON = filepath
+
 	if cfg.TelegramToken == "" || cfg.CredentialsJSON == "" {
 		log.Fatal("❌ Missing required environment variables")
 	}
 	return cfg
+}
+
+func WriteCredentialsToTempFile(b64 string) string {
+
+	data, _ := base64.StdEncoding.DecodeString(b64)
+
+	tmpFile, err := os.CreateTemp("", "firebase-*.json")
+	if err != nil {
+		log.Fatalf("❌ Failed to create temp file: %v", err)
+	}
+	tmpFile.Write(data)
+	tmpFile.Close()
+
+	return tmpFile.Name()
 }
